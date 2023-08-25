0bafa510566bb158abe63e234919f6432d6af606
Dino1984WB <williamkbukowkidev@gmail.com>
2023-08-25 19:49:37 -0400

    added game logic in js

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+// Dino
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-const EXPLOSION_RADIUS = 30;
+const EXPLOSION_RADIUS = 50;
 
 let missiles = [];
 let counterMissiles = [];
 let explosions = [];
 
 class Missile {
-    constructor(x, y, targetX, targetY) {
+    constructor(x, y, targetX, targetY, speed = 3) {
         this.x = x;
         this.y = y;
         this.targetX = targetX;
         this.targetY = targetY;
+        this.speed = speed;
+    }
+
+    update() {
+        const angle = Math.atan2(this.targetY - this.y, this.targetX - this.x);
+        this.x += this.speed * Math.cos(angle);
+        this.y += this.speed * Math.sin(angle);
     }
 
     draw() {
@@ -22,67 +31,68 @@ class Missile {
     }
 }
 
-class CounterMissile extends Missile {
+class Explosion {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
     draw() {
-        ctx.fillStyle = "red";
+        ctx.fillStyle = "yellow";
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, EXPLOSION_RADIUS, 0, Math.PI * 2);
         ctx.fill();
     }
 }
 
-class Explosion {
-    constructor(x, y, maxRadius=30) {
-        this.x = x;
-        this.y = y;
-        this.radius = 0;
-        this.maxRadius = maxRadius;
-    }
-
-    update() {
-        this.radius++;
-        return this.radius >= this.maxRadius;
-    }
-
-    draw() {
-        ctx.fillStyle = "#39FF14";
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
+function detectCollisions() {
+    for (const explosion of explosions) {
+        missiles = missiles.filter(missile => {
+            const dist = Math.hypot(explosion.x - missile.x, explosion.y - missile.y);
+            return dist > EXPLOSION_RADIUS;
+        });
     }
 }
 
 function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Insert your logic for generating missiles and counter-missiles here
+    // Generate new missile occasionally
+    if (Math.random() < 0.02) {
+        const x = Math.random() * canvas.width;
+        const targetX = Math.random() * canvas.width;
+        missiles.push(new Missile(x, 0, targetX, canvas.height));
+    }
 
     // Update and draw missiles
     for (const missile of missiles) {
+        missile.update();
         missile.draw();
     }
 
     // Update and draw counter-missiles
     for (const counterMissile of counterMissiles) {
+        counterMissile.update();
         counterMissile.draw();
     }
 
     // Update and draw explosions
     for (const explosion of explosions) {
-        if (explosion.update()) {
-            // Remove explosion when it has reached max size
-            const index = explosions.indexOf(explosion);
-            if (index > -1) {
-                explosions.splice(index, 1);
-            }
-        }
         explosion.draw();
     }
 
-    // Check for missile and explosion intersections here
+    // Detect and handle collisions
+    detectCollisions();
 
     requestAnimationFrame(gameLoop);
 }
 
-// Start the game loop
+canvas.addEventListener('click', function(event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    counterMissiles.push(new Missile(canvas.width / 2, canvas.height, x, y, 4));
+    explosions.push(new Explosion(x, y));
+});
+
 gameLoop();
